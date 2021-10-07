7487e07e5126a1dd2e94b4b5ebc75a88c52c9d50
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-07 18:40:02 +0530

    Initial commit

---
+++ main.sql
@@ -0,0 +1,3 @@
+SQL is a domain-specific language used in programming and designed for managing
+data held in a relational database management system, or for stream processing
+in a relational data stream management system.
